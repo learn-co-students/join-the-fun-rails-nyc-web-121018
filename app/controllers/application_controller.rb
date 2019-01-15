1193072d83a3dd66221b8acad902b6c2b0eab744
EvanPavley <github email address>
2019-01-15 11:39:17 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,3 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 end
+
+# rails g migration add_taxi_id_to_rides taxi_id:integer --no-test-framework
+# rails g migration add_passenger_id_to_rides passenger_id:integer --no-test-framework
